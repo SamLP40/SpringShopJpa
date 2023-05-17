b64b4808159f608e25a18952bd382318587a4229
SamSam <sam40230@gmail.com>
2023-05-17 10:04:14 +0200

    added spring framework + saved items in DB

--- src/main/java/fr/fms/SpringShopJpaApplication.java
+++ src/main/java/fr/fms/SpringShopJpaApplication.java
@@ -2,6 +2,13 @@ package fr.fms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import fr.fms.dao.ArticleDao;
+import fr.fms.dao.CategoryRepository;
+import fr.fms.entities.Article;
+import fr.fms.entities.Category;
 
 @SpringBootApplication
 public class SpringShopJpaApplication {
